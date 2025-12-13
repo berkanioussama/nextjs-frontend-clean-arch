b90c3b9b1d3f7d69c867a4b1272e5c13776ff89d
Berkani oussama ala eddine <berkanioussama@gmail.com>
2025-12-13 22:44:18 +0100

    add sign in button to home

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Page from "@/shared/ui/components/layout/page";
 import Container from "@/shared/ui/components/layout/container";
 import { Button } from "@/shared/ui/components/ui/button";
 import Link from "next/link";
-import { SignOutButton } from "@clerk/nextjs";
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
@@ -15,13 +15,15 @@ export default function Home() {
       <Container className="min-h-svh flex flex-col gap-6 items-center justify-center">
         <h1 className="text-4xl font-bold">Home Page</h1>
         <p className="text-lg">You can see this page without logging in</p>
-
-        {user &&
-        <>
-          <Link href="/profile"><Button>Go to profile</Button></Link>
-          <SignOutButton><Button variant="destructive">Log out</Button></SignOutButton>
-        </>
-        }
+        <SignInButton>
+          <Button>Sign in</Button>
+        </SignInButton>
+        {user && (
+          <>
+            <Link href="/profile"><Button>Go to profile</Button></Link>
+            <SignOutButton><Button variant="destructive">Log out</Button></SignOutButton>
+          </>
+        )}
       </Container>
     </Page>
   );
